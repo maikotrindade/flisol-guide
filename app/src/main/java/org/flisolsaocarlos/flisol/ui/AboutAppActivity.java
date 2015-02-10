98e707402b47b909476c848483ff8e386b0f9bb9
Maiko Trindade <maikocharles@gmail.com>
2015-02-10 02:09:52 -0200

    about the app: add sending e-mail to the developer
    add a validated sending e-mail

--- app/src/main/java/org/flisolsaocarlos/flisol/ui/AboutAppActivity.java
+++ app/src/main/java/org/flisolsaocarlos/flisol/ui/AboutAppActivity.java
@@ -7,15 +7,30 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.flisolsaocarlos.flisol.BuildConfig;
 import org.flisolsaocarlos.flisol.R;
 
-public class AboutAppActivity extends Activity implements View.OnClickListener{
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AboutAppActivity extends Activity implements View.OnClickListener {
+
+    final static int MAX_NAME_LENGHT = 70;
+    final static int MAX_SUBJECT_LENGHT = 150;
+    final static int MAX_MESSAGE_LENGHT = 400;
+    final static String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     private TextView versionNameTxt;
-    private Button licenseBtn;
+    private Button licenseBtn, submitBtn;
+    private EditText nameEdt, emailEdt, subjectEdt, messageEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,14 @@ public class AboutAppActivity extends Activity implements View.OnClickListener{
         licenseBtn = (Button) findViewById(R.id.licenseBtn);
         licenseBtn.setOnClickListener(this);
 
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(this);
+
+        nameEdt = (EditText) findViewById(R.id.nameEdit);
+        emailEdt = (EditText) findViewById(R.id.emailEdit);
+        subjectEdt = (EditText) findViewById(R.id.subjectEdit);
+        messageEdt = (EditText) findViewById(R.id.messageEdit);
+
     }
 
     public void onClick(View view) {
@@ -42,9 +65,70 @@ public class AboutAppActivity extends Activity implements View.OnClickListener{
                 i.setData(Uri.parse(licenseUrl));
                 startActivity(i);
                 break;
+            case R.id.submitBtn:
+
+                final String name = nameEdt.getText().toString();
+                if ( (name.isEmpty()) && (name.length() < MAX_NAME_LENGHT) ) {
+                    nameEdt.setError(getResources().getString(R.string.invalid_name));
+                    break;
+                }
+
+                final String email = emailEdt.getText().toString();
+                if (!isValidEmail(email)) {
+                    emailEdt.setError(getResources().getString(R.string.invalid_email));
+                    break;
+                }
+
+                final String subject = subjectEdt.getText().toString();
+                if ( (subject.isEmpty()) && (subject.length() < MAX_SUBJECT_LENGHT) ) {
+                    subjectEdt.setError(getResources().getString(R.string.invalid_subject));
+                    break;
+                }
+
+                final String message = messageEdt.getText().toString();
+                if ( (message.isEmpty()) && (message.length() < MAX_MESSAGE_LENGHT) ) {
+                    messageEdt.setError(getResources().getString(R.string.invalid_message));
+                    break;
+                }
+
+                submitEmail(email, name, subject, message);
+                break;
         }
     }
 
+    private void submitEmail(final String email, final String name,
+                             final String subject, final String message) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        final String developerEmail = getResources().getString(R.string.developer_email);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{developerEmail});
+
+        StringBuilder subjectBuilder = new StringBuilder();
+        subjectBuilder.append("[FLISOL - ").append(subject).append(" ]");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subjectBuilder.toString());
+
+        final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        final Calendar cal = Calendar.getInstance();
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append("Sent by ").append(name).append(" ")
+                .append("<").append(email).append(">\n\n")
+                .append(message).append("\n\n")
+                .append("Date: ").append(dateFormat.format(cal.getTime()));
+        intent.putExtra(Intent.EXTRA_TEXT, messageBuilder.toString());
+
+        try {
+            startActivity(Intent.createChooser(intent, getResources().getString(R.string.send_email)));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(AboutAppActivity.this, getResources().getString(R.string.send_email_error), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -70,3 +154,5 @@ public class AboutAppActivity extends Activity implements View.OnClickListener{
         overridePendingTransition(R.anim.end_in, R.anim.end_out);
     }
 }
+
+
