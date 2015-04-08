408afe2699f64b1f446f19ad3890a82b2d583b2e
Maiko Trindade <maikocharles@gmail.com>
2015-04-07 22:15:56 -0300

    agenda: add tab structure

--- app/src/main/java/org/flisolsaocarlos/flisol/ui/AgendaActivity.java
+++ app/src/main/java/org/flisolsaocarlos/flisol/ui/AgendaActivity.java
@@ -1,8 +1,9 @@
 package org.flisolsaocarlos.flisol.ui;
 
-import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.MenuItem;
 
 import org.flisolsaocarlos.flisol.R;
@@ -11,19 +12,28 @@ import de.keyboardsurfer.android.widget.crouton.Configuration;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
-public class AgendaActivity extends Activity {
+public class AgendaActivity extends FragmentActivity {
 
+    private FragmentTabHost mTabHost;
     private Crouton crouton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.schedule_activity);
+        setContentView(R.layout.agenda_activity);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setIcon(R.drawable.ic_launcher_white);
 
-        //TODO implement schedule and remove this message
-        userFeedback();
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Tab1"),
+                AgendaRoomOneFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Tab2"),
+                AgendaRoomOneFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("tab3").setIndicator("Tab3"),
+                AgendaRoomOneFragment.class, null);
+
     }
 
     @Override
